5b5bf789361347216fa5a5c8fdb7a4dc03cb4d58
praj0015 <43075555+praj0015@users.noreply.github.com>
2018-11-14 14:51:23 -0800

    create function

--- js/main.js
+++ js/main.js
@@ -14,6 +14,55 @@ function init() {
 
 function addEventListeners() {
 
+    document.querySelector(".search-button").addEventListener("click", showoverlay);
+
+    document.querySelector(".btncancel").addEventListener("click", hideoverlay);
+
+    document.querySelector(".btnsave").addEventListener("click", function (e) {
+
+        let TVshow = document.getElementsByName("show");
+        let Showlist = null;
+        for (let i = 0; i < TVshow.length; i++) {
+            if (TVshow[i].checked) {
+                Showlist = TVshow[i].value;
+                break;
+            }
+        }
+        console.log(Showlist);
+        hideoverlay(e);
+    });
+}
+
+function showoverlay(e) {
+    e.preventDefault();
+    let overlay = document.querySelector(".overlay");
+    overlay.classList.add("show");
+    overlay.classList.remove("hide");
+    showmodal(e);
+}
+
+function showmodal(e) {
+
+    e.preventDefault();
+    let modal = document.querySelector(".modal");
+    modal.classList.add("on");
+    modal.classList.remove("off");
+}
+
+function hideoverlay(e) {
+    e.preventDefault();
+    let overlay = document.querySelector(".overlay");
+    overlay.classList.add("hide");
+    overlay.classList.remove("show");
+    hidemodal(e);
+}
+
+function hidemodal(e) {
+    e.preventDefault();
+    e.stopPropagation(); // do not allow click to pass through
+    let modal = document.querySelector(".modal");
+    modal.classList.add("off");
+    modal.classList.remove("on");
 }
 
 function getDataFromLocalStorage() {
@@ -30,8 +79,8 @@ function getPosterURLAndSizes() {
     //https://api.themoviedb.org/3/configuration?api_key=<<api_key>>
 
     let url = `${movieDataBaseURL}configuration?api_key=${APIKEY}`;
-    
-    let img=document
+
+    let img = document
 
     //fetch
 
@@ -44,7 +93,7 @@ function getPosterURLAndSizes() {
 
             imageURL = data.images.secure_base_url;
             imagesizes = data.images.poster_sizes;
-        
+
             console.log(imageURL);
             console.log(imagesizes);
         })
